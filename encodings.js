25099c1dafd53912802697f6aea8be5c920839d2
pgoldthorpe <43900257+pgoldthorpe@users.noreply.github.com>
2020-02-21 23:42:17 +1100

    Added more user options

--- encodings.js
+++ encodings.js
@@ -1,13 +1,9 @@
-function createMatrix(rows, cols) {
-  return new Array(rows).fill(0).map(() => new Array(cols).fill(0));
-}
-
-var data = createMatrix(8,8);
+var data = createMatrix(30, 30);
 
 var width = 500,
   height = 500
-  radius = 40;
-  spacing = 60;
+  radius = 10;
+  spacing = 15;
 
 var svg = d3.select('body').select('svg')
   .attr('height', height)
@@ -15,15 +11,17 @@ var svg = d3.select('body').select('svg')
   .append('g')
   .attr('transform', 'translate('+[0, spacing]+')');
 
-
 var row = svg.selectAll('.row').data(data)
 .join(
   enter => enter.append('g')
   .attr('transform', (d,i) => 'translate(' + [spacing/2, i * spacing - 3*spacing/4] + ')')
   )
 
-
 create()
+
+function createMatrix(rows, cols) {
+  return new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+}
 
 function create() {
   row.selectAll('rect')
@@ -38,7 +36,7 @@ function create() {
       .attr('fill','black')
       .attr('transform','rotate(0)'),
     update => update
-    .transition("move").duration(1000)
+    .transition("move").duration(2000)
       .attr('x', (d,i) => i * spacing - spacing/4)
       .attr('y', 0)
       .attr('width', radius)
@@ -49,34 +47,63 @@ function create() {
     )
   }
 
-function update() {
+function update(rand=5) {
   tt = +d3.select('input').property('value')
-  index1 = Math.floor(Math.random()*data.length * data[0].length);
-  index2 = Math.floor(Math.random()*data.length * data[0].length);
-  index3 = Math.floor(Math.random()*data.length * data[0].length);
-  index4 = Math.floor(Math.random()*data.length * data[0].length);
-  index5 = Math.floor(Math.random()*data.length * data[0].length);
-  rand = Math.floor(Math.random()*360);
+  easetype = window["d3"][d3.select('select').property('value')]
   
-  xval = +d3.select(d3.selectAll('rect')._groups[0][index2]).attr('x')
-  yval = +d3.select(d3.selectAll('rect')._groups[0][index2]).attr('y')
-  fun = window["d3"][d3.select('select').property('value')] //choose the ease
-  shift = d3.select('#shift').property('value')
   if (d3.select('#grey').property('checked')) {
-    d3.select(d3.selectAll('rect')._groups[0][index1]).transition("fill").duration(tt).ease(fun).attr('fill','lightgrey')}
+    randElements = getRandom(d3.selectAll('rect')["_groups"][0], rand)
+    d3.selectAll(randElements)
+      .transition("fill")
+      .duration(tt).ease(easetype).attr('fill','lightgrey')
+    }
+  
   if (d3.select('#move').property('checked')) {
-    d3.select(d3.selectAll('rect')._groups[0][index2]).transition("move").duration(tt).ease(fun).attr('y', yval + shift*Math.sin(rand)).attr('x', xval + shift*Math.cos(rand))}
+    randElements = getRandom(d3.selectAll('rect')["_groups"][0], rand)
+    shift = d3.select('#shift').property('value')
+    d3.selectAll(randElements)
+      .data(randElements)
+      .transition("move")
+      .duration(tt).ease(easetype)
+      .attr('x', (d) => d.x.baseVal.value + shift * Math.cos(Math.floor(Math.random()*360)))
+      .attr('y', (d) => d.y.baseVal.value + shift * Math.sin(Math.floor(Math.random()*360)))
+    }
+  
   if (d3.select('#shape').property('checked')) {
-    d3.select(d3.selectAll('rect')._groups[0][index3]).transition("shape").duration(tt).ease(fun).attr('rx', radius/2)}
+    randElements = getRandom(d3.selectAll('rect')["_groups"][0], rand)
+    d3.selectAll(randElements)
+      .transition("shape")
+      .duration(tt).ease(easetype)
+      .attr('rx', radius/2)
+    }
+
   if (d3.select('#red').property('checked')) {
-    d3.select(d3.selectAll('rect')._groups[0][index5]).transition("fill").duration(tt).ease(fun).attr('fill', d3.select('#picker').property('value'))}
-}
+    randElements = getRandom(d3.selectAll('rect')["_groups"][0], rand)
+    d3.selectAll(randElements)
+      .transition("fill")
+      .duration(tt).ease(easetype)
+      .attr('fill', d3.select('#picker').property('value'))
+    }
+  }
 
 function dbl() {
-  update();update();update();update();update();update();update();update();update();update();
-  update();update();update();update();update();update();update();update();update();update();
+  update(rand=100)
 }
 
 function updatetext() {
   d3.select('slidertext').text(d3.select('#shift').property('value'))
+}
+
+function getRandom(arr, n) {
+  var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+  if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
 }
